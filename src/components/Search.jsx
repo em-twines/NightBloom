f8dee39cf7eb8ad495c18f8104b50268eb2ee881
em-twines <emdelbridge@gmail.com>
2023-04-27 14:44:19 -0400

    working on search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import SearchIcon from "../media/icon.svg";
 
 export default function Search() {
   return (
-    <div className = 'search'>Search</div>
-  )
+    <div className="search">
+      <form>
+        <input type="search" placeholder="Keyword search"></input>
+      </form>
+    </div>
+  );
 }
