cf09b75bd96d5d97ce1bb09d8672fa6c2fa90e94
em-twines <emdelbridge@gmail.com>
2023-04-28 09:43:03 -0400

    layout of search bar correct

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,7 +4,10 @@ import SearchIcon from "../media/icon.svg";
 export default function Search() {
   return (
     <div className="search">
-      <form>
+      <form className = 'search-form'>
+        <div className = 'padding-container'>
+          <div className = 'search-background'></div>
+        </div>
         <input type="search" placeholder="Keyword search"></input>
       </form>
     </div>
