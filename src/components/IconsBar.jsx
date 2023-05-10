d0ac6629c66185132d75c91f8e33d65ab7d872c1
em-twines <emdelbridge@gmail.com>
2023-05-09 21:09:30 -0400

    icon button styling complete

--- src/components/IconsBar.jsx
+++ src/components/IconsBar.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 
+import hot from "../media/hot.svg";
+import newIcon from '../media/new.svg';
+import top from '../media/star.svg';
+
+
 export default function IconsBar() {
   return (
     <div className="icons-container">
-      <button className="icons-left icons-button">Hot</button>
+      <div className="icons-left-container">
+        <div className="icons-left icons-button">
+          <img src={hot}/>
+          <button className = 'icons-button-span'> Hot</button>
+        </div>
+        <div className="icons-left icons-button">
+          <img src={newIcon}/>
+          <button className = 'icons-button-span'> New</button>
+        </div>
+        <div className="icons-left icons-button">
+          <img src={top}/>
+          <button className = 'icons-button-span'> Top</button>
+        </div>
+      </div>
       <select className="icons-right icons-select">
         <option value="">All</option>
         <option value="dog">Dog</option>
